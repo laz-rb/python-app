0289fd68c90f7862441e760759aa846b6348e286
Eleazar Ponce <poncebe@gmail.com>
2025-12-08 11:45:38 -0600

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,19 @@
+import datetime
+import socket
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+    return jsonify({
+        'date': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'hostname': socket.gethostname()
+    })
+
+@app.route('/api/v1/healthz')
+def healthz():
+    return jsonify({'status': 'ok'}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
